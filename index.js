d75109f9f28dba39440dd0237a46386d589fd4ec
sutedalm <37000551+sutedalm@users.noreply.github.com>
2021-11-20 15:18:35 +0100

    initial hello world server

---
+++ index.js
@@ -0,0 +1,17 @@
+const app = require("express")();
+const httpServer = require("http").Server(app);
+// const io = require("socket.io")(http);
+const io = require("socket.io")(httpServer, {
+  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+});
+const port = process.env.PORT || 8080;
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg + " World!");
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
